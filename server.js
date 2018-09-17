b22181f7bf2397f35e393e11bdd969d5aeab28ef
jidapa <onjidapa@windowslive.com>
2018-09-17 18:00:23 +0700

    edit

--- server.js
+++ server.js
@@ -13,29 +13,8 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
   user     : 's140390',
-  password : 'abc123**',
-  database : 'db140390'
-});
-
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
-
-app.get('/students', function (req, res) {
-    connection.connect()
-    connection.query('SELECT * students', function (err, rows, fields) {
-        if (err) throw err
-      
-        console.log('The solution is: ', rows[0].solution)
-      })
-      connection.end()
-    
+  password : 'abc123',
+  database : 'my_db'
 });
 
 app.listen(2222);
