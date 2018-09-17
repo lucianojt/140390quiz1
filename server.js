fb6703414cd5c323d29efe8d978786a3d1cb1b2d
jidapa <onjidapa@windowslive.com>
2018-09-17 18:34:28 +0700

    finish

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var app = express();
-
+var mysql = require('mysql')
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (request, response) {
-    response.render('pages/index');
+app.get('/', function (request, res) {
+    res.render('pages/index');
 
 });
 
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host     : 'www.db4free.net',
-  user     : 's140390',
-  password : 'abc123',
-  database : 'my_db'
+app.get('/students', function (req, res) {
+    var connection = mysql.createConnection({
+        host     : 'www.db4free.net',
+        user     : 's140390',
+        password : 'abc123**',
+        database : 'db140390'
+    });
+    connection.connect()
+    connection.query('SELECT * FROM students', function (err, rows, fields) {
+        if (err) throw err
+        res.render('pages/students',{students:rows});
+
+    });
 });
 
-app.listen(2222);
+app.get('/subjects', function (req, res) {
+    var connection = mysql.createConnection({
+        host     : 'www.db4free.net',
+        user     : 's140390',
+        password : 'abc123**',
+        database : 'db140390'
+    });
+    connection.connect()
+    connection.query('SELECT * FROM subjects', function (err, rows, fields) {
+        if (err) throw err
+        res.render('pages/subjects',{subjects:rows});
+
+    });
+});
+
+
+app.listen(8080);
